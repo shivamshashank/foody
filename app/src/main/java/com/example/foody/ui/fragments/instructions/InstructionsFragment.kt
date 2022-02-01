67b5d40d5937c2371c4ec47489bf7b22a32310bf
shivamshashank <shivamkumar872000@gmail.com>
2022-02-01 22:43:10 +0530

    Fragment Instructions Completed.

--- app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,20 +1,34 @@
 package com.example.foody.ui.fragments.instructions
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foody.R
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.foody.databinding.FragmentInstructionsBinding
+import com.example.foody.models.food_recipe.Result
+import com.example.foody.util.Constants
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        binding.instructionsWebView.loadUrl(myBundle!!.sourceUrl)
+
+        return binding.root
     }
 
 }
